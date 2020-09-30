7fc6062e94f83de6bcf82c931c7583f3cbc11a4c
RSD-accessboard <55765201+RSD-accessboard@users.noreply.github.com>
2020-09-30 16:04:27 -0400

    Toast added and working

--- _assets/js/anchorify.js
+++ _assets/js/anchorify.js
@@ -27,7 +27,17 @@ $( document ).ready(function() {
     document.body.removeChild(temp);
 
     //visible alert
-    alert('Copied Link to Clipboard');
+    $.toast({ 
+      text : "Link copied to clipboard", 
+      showHideTransition : 'slide',  // It can be plain, fade or slide
+      bgColor : 'blue',              // Background color for toast
+      textColor : '#eee',            // text color
+      allowToastClose : true,       // Show the close button or not
+      hideAfter : 5000,              // `false` to make it sticky or time in miliseconds to hide after
+      stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+      textAlign : 'left',            // Alignment of text i.e. left, right, center
+      position : 'bottom-left'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+    });
 
   });
 });
