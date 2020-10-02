7648438e702861b45a613f71e47a38acc6906357
RSD-accessboard <55765201+RSD-accessboard@users.noreply.github.com>
2020-10-02 11:05:39 -0400

    improved anchors

--- _assets/js/anchorify.js
+++ _assets/js/anchorify.js
@@ -1,10 +1,14 @@
 (function() {
   const anchors = new AnchorJS({
     placement: "left",
-    ariaLabel: "Copy URL"
+    //ariaLabel: "Copy URL",
+    //class: "",
+    //placement: "right",
+    ariaLabel: "copy me",
+    visible: "always",
   });
 
-  anchors.add();
+  anchors.add('.anchored h2, .anchored h3, .anchored h4, .anchored h5, .anchored h6');
 })();
 
 $( document ).ready(function() {
@@ -30,13 +34,14 @@ $( document ).ready(function() {
     $.toast({ 
       text : "Link copied to clipboard", 
       showHideTransition : 'slide',  // It can be plain, fade or slide
-      bgColor : 'blue',              // Background color for toast
-      textColor : '#eee',            // text color
+      bgColor : '#1a4480',              // Background color for toast
+      textColor : '#ffffff',            // text color
       allowToastClose : true,       // Show the close button or not
-      hideAfter : 5000,              // `false` to make it sticky or time in miliseconds to hide after
+      hideAfter : 3000,              // `false` to make it sticky or time in miliseconds to hide after
       stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
       textAlign : 'left',            // Alignment of text i.e. left, right, center
-      position : 'bottom-left'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+      position : 'bottom-left',       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+      role : 'alert'
     });
 
   });
