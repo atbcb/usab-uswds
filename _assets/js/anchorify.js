e94855f19ad7ca3469ff0aef7e018f19b09ddbf8
RSD-accessboard <55765201+RSD-accessboard@users.noreply.github.com>
2020-10-06 16:00:45 -0400

    toast alert

--- _assets/js/anchorify.js
+++ _assets/js/anchorify.js
@@ -37,11 +37,11 @@ $( document ).ready(function() {
       bgColor : '#1a4480',              // Background color for toast
       textColor : '#ffffff',            // text color
       allowToastClose : true,       // Show the close button or not
-      hideAfter : 3000,              // `false` to make it sticky or time in miliseconds to hide after
+      hideAfter : false,              // `false` to make it sticky or time in miliseconds to hide after
       stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
       textAlign : 'left',            // Alignment of text i.e. left, right, center
-      position : 'bottom-left',       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
-      role : 'alert'
+      position : 'bottom-left'      // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+
     });
 
   });
