d724ce58fae679854022ca8c6a6ebdd87d81cc13
RSD-accessboard <55765201+RSD-accessboard@users.noreply.github.com>
2020-11-23 12:55:07 -0500

    npm install swiper

---
+++ _assets/js/swiper.js
@@ -0,0 +1,27 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// init Swiper:
+const swiper = new Swiper(...);
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
