aa212df89325c25b899feff4c6c5ad8ae9f99fda
Kathy Eng <kengdoj@users.noreply.github.com>
2025-11-20 19:05:59 +0000

    fix trim

--- _plugins/asset-helpers.rb
+++ _plugins/asset-helpers.rb
@@ -3,18 +3,31 @@ require 'fileutils'
 module AssetHelper
     def self.process(site, payload)
         return if @processed
-        FileUtils.cp_r('node_modules/uswds/dist/js/', 'assets/', verbose: true)
-        FileUtils.cp_r('node_modules/uswds/dist/img/', 'assets/uswds/', verbose: true)
-        FileUtils.cp_r('node_modules/uswds/dist/fonts/', 'assets/uswds/', verbose: true)
-        FileUtils.cp_r('node_modules/netlify-cms/dist/', 'assets/netlify-cms/', verbose: true)
-        FileUtils.cp_r('node_modules/jquery/dist/', 'assets/jquery/', verbose: true)
-        FileUtils.cp_r('node_modules/swiper/', 'assets/swiper/', verbose: true)
-        FileUtils.cp_r('node_modules/video.js/dist/', 'assets/video-js/', verbose: true)
-        FileUtils.cp_r('node_modules/stickyfilljs/dist/', 'assets/stickyfilljs/', verbose: true)
-        FileUtils.cp_r('node_modules/videojs-youtube/dist/', 'assets/youtube-video-js/', verbose: true)
-        FileUtils.cp_r('node_modules/@fortawesome/fontawesome-free/', 'assets/fontawesome-free/', verbose: true)
-        FileUtils.cp_r('node_modules/jquery-toast-plugin/dist/', 'assets/jquery-toast-plugin/', verbose: true)
-        FileUtils.cp_r('node_modules/dompurify/dist/', 'assets/dompurify/', verbose: true)
+        def self.cp_safe(src, dest)
+            if Dir.exist?(src)
+                FileUtils.mkdir_p(dest) unless Dir.exist?(dest)
+                begin
+                    FileUtils.cp_r(src, dest, verbose: true)
+                rescue Errno::ENOENT => e
+                    puts "AssetHelper: copy failed #{src} -> #{dest}: #{e.class}: #{e.message}"
+                end
+            else
+                puts "AssetHelper: source #{src} not found; skipping"
+            end
+        end
+
+        cp_safe('node_modules/uswds/dist/js/', 'assets/')
+        cp_safe('node_modules/uswds/dist/img/', 'assets/uswds/')
+        cp_safe('node_modules/uswds/dist/fonts/', 'assets/uswds/')
+        cp_safe('node_modules/netlify-cms/dist/', 'assets/netlify-cms/')
+        cp_safe('node_modules/jquery/dist/', 'assets/jquery/')
+        cp_safe('node_modules/swiper/', 'assets/swiper/')
+        cp_safe('node_modules/video.js/dist/', 'assets/video-js/')
+        cp_safe('node_modules/stickyfilljs/dist/', 'assets/stickyfilljs/')
+        cp_safe('node_modules/videojs-youtube/dist/', 'assets/youtube-video-js/')
+        cp_safe('node_modules/@fortawesome/fontawesome-free/', 'assets/fontawesome-free/')
+        cp_safe('node_modules/jquery-toast-plugin/dist/', 'assets/jquery-toast-plugin/')
+        cp_safe('node_modules/dompurify/dist/', 'assets/dompurify/')
         @processed = true
     end
 end
